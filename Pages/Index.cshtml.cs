e00a212bab4bbbc837cd722ed5c427a23bdadf17
MariamEssam553 <mariamessammohamed98@gmail.com>
2023-07-12 01:26:49 +0300

    trying to implement cookies

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,20 +1,71 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Xml;
 
-namespace HW5.Pages
+namespace HW5.Pages;
+
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
+    public List<FeedItem> items4page { get; set; } = new();
+    public List<FeedItem> outlines { get; set; } = new();
+
+    private readonly IHttpClientFactory _httpClientFactory;
+    public IndexModel(IHttpClientFactory httpClientFactory) =>
+        _httpClientFactory = httpClientFactory;
+    public async Task OnGetAsync(int pageNumber = 1, int pageSize = 12)
     {
-        private readonly ILogger<IndexModel> _logger;
+        var client = _httpClientFactory.CreateClient();
+        var response = await client.GetAsync("https://blue.feedland.org/opml?screenname=dave");
 
-        public IndexModel(ILogger<IndexModel> logger)
+        string OPMLcontent = await response.Content.ReadAsStringAsync();
+
+        XmlDocument opmlDocument = new XmlDocument();
+        opmlDocument.LoadXml(OPMLcontent);
+        XmlElement? root = opmlDocument.DocumentElement;
+        XmlNodeList nodes = root.GetElementsByTagName("outline");
+        List<FeedItem> itemsList = new List<FeedItem>();
+
+        int totalItems = nodes?.Count ?? 0;
+        int totalPages = (int)Math.Ceiling((double)totalItems / pageSize);
+
+        int startIndex = (pageNumber - 1) * pageSize;
+        int endIndex = Math.Min(startIndex + pageSize, nodes.Count);
+
+
+        foreach (XmlNode node in nodes)
         {
-            _logger = logger;
+            string Text = node.Attributes["text"].Value ?? "";
+            string link = node.Attributes["xmlUrl"].Value ?? "";
+
+            FeedItem newItem = new FeedItem()
+            {
+                Text = Text,
+                XmlLink = link
+            };
+
+            itemsList.Add(newItem);
         }
 
-        public void OnGet()
-        {
+        outlines = itemsList;
 
-        }
+        // Assuming 'allItems' is the complete list of items
+        var itemsForPage = outlines.Skip(startIndex).Take(endIndex - startIndex).ToList();
+
+        items4page = itemsForPage;
+
+        ViewData["PageNumber"] = pageNumber;
+        ViewData["PageSize"] = pageSize;
+        ViewData["TotalItems"] = totalItems;
+        ViewData["TotalPages"] = totalPages;
+
     }
+
 }
+
+public class FeedItem
+{
+    public string? Text { get; set; }
+    public string? XmlLink { get; set; }
+    public string IsFavorite { get; set; } = "False";
+}
+
+
---
+++ Pages/XMLpage.cshtml.cs
@@ -0,0 +1,75 @@
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Xml;
+
+namespace HW5.Pages;
+
+public class XMLpageModel : PageModel
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public XMLpageModel(IHttpClientFactory httpClientFactory) =>
+        _httpClientFactory = httpClientFactory;
+
+    public List<Item> Items { get; set; } = new();
+    public async Task<IActionResult> OnGetAsync()
+    {
+        string feed = Request.Query["message"];
+        await renderXml(feed);
+        return Page();
+    }
+
+    public async Task renderXml(string url)
+    {
+        //string url = "http://scripting.com/rss.xml";
+
+        var client = _httpClientFactory.CreateClient();
+        var response = await client.GetAsync(url);
+        string XMLcontent = await response.Content.ReadAsStringAsync();
+
+        XmlDocument xmlDocument = new XmlDocument();
+        xmlDocument.LoadXml(XMLcontent);
+        XmlElement? root = xmlDocument.DocumentElement;
+        XmlNodeList nodes = root.GetElementsByTagName("item");
+        List<Item> itemsList = new List<Item>();
+
+        foreach (XmlNode node in nodes)
+        {
+            XmlNode? TitleTag = node.SelectSingleNode("title");
+            string Title = (TitleTag != null) ? TitleTag.InnerText : "";
+
+            XmlNode? DescriptionTag = node.SelectSingleNode("description");
+            string htmlCode = (DescriptionTag != null) ? DescriptionTag.InnerText : "";
+            HtmlString Description = new HtmlString(htmlCode);
+
+            XmlNode? PubDateTag = node.SelectSingleNode("pubDate");
+            string PubDate = (PubDateTag != null) ? PubDateTag.InnerText : "";
+            var date = DateTime.Parse(PubDate);
+
+            XmlNode? LinkTag = node.SelectSingleNode("link");
+            string Link = (LinkTag != null) ? LinkTag.InnerText : "";
+
+            Item newItem = new Item()
+            {
+                Title = Title,
+                Description = Description,
+                PubDate = date,
+                Link = Link
+            };
+
+            itemsList.Add(newItem);
+        }
+
+        Items = itemsList;
+
+
+    }
+}
+public class Item
+{
+    public string? Title { get; set; }
+    public HtmlString? Description { get; set; }
+    public DateTime PubDate { get; set; }
+    public string? Link { get; set; }
+}
